323ceb1cb427a0938f748b19328df8efe8c6a1c0
schae42 <chaechan@outlook.com>
2020-11-01 18:04:56 -0800

    first JSX

---
+++ my-first-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HelloDojoComponent from './components/HelloDojo';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloDojoComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-project/src/components/HelloDojo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class HelloDojoComponent extends Component {
+    render() {
+        return (
+        <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <p>* Learn React</p>
+        <p>* Climb Mt. Everest</p>
+        <p>* Run a marathon</p>
+        <p>* Feed the dogs</p>
+        </div>
+        );
+    };
+};
+export default HelloDojoComponent;
